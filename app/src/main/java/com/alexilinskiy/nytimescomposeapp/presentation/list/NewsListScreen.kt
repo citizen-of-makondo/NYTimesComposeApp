947985a906659f47edd3c2315fab64468aa87e12
Alexandr Ilinskiy <alexandr.rondyrev-ilinskiy@touchin.ru>
2022-06-16 16:59:21 +0300

    Add Glide for picture

--- app/src/main/java/com/alexilinskiy/nytimescomposeapp/presentation/list/NewsListScreen.kt
+++ app/src/main/java/com/alexilinskiy/nytimescomposeapp/presentation/list/NewsListScreen.kt
@@ -1,16 +1,17 @@
 package com.alexilinskiy.nytimescomposeapp.presentation.list
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.alexilinskiy.nytimescomposeapp.model.Result
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
@@ -26,6 +27,28 @@ fun NewsListScreen(
 }
 
 @Composable
-fun NewsItem(newsItem: Result?) {
-    Text(text = newsItem?.title.orEmpty())
+fun NewsItem(newsItem: Result) {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+
+    ) {
+        GlideImage(
+            imageModel = newsItem.multimedia.first().url,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier.weight(0.2f)
+                .padding(8.dp),
+            requestOptions = {
+                RequestOptions()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .centerCrop()
+            },
+            alignment = Alignment.Center
+        )
+        Column(
+            modifier = Modifier.weight(0.8f)
+                .padding(start = 8.dp)
+        ) {
+            Text(text = newsItem.title)
+        }
+    }
 }
